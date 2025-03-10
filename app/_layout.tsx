9c8d09614ff4b5bbf581c134b7587d9c11a295e8
Conal Ryan <conal_ryan_2@hotmail.com>
2025-03-10 15:48:59 +1300

    Change layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import AppNavigation from '../navigation/AppNavigation';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
@@ -50,10 +51,7 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+      <AppNavigation/>
     </ThemeProvider>
   );
 }
